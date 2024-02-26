79446ce6fdb077f9e4985479330057a11f98e6b5
Muhiddin <muhiddinjv@gmail.com>
2024-02-26 11:02:55 +0500

    initial commit

---
+++ src/html/script.js
@@ -0,0 +1,300 @@
+const baqara282arab =
+  "يَـٰٓأَيُّهَا ٱلَّذِينَ ءَامَنُوٓا۟ إِذَا تَدَايَنتُم بِدَيْنٍ إِلَىٰٓ أَجَلٍۢ مُّسَمًّۭى فَٱكْتُبُوهُ ۚ وَلْيَكْتُب بَّيْنَكُمْ كَاتِبٌۢ بِٱلْعَدْلِ ۚ وَلَا يَأْبَ كَاتِبٌ أَن يَكْتُبَ كَمَا عَلَّمَهُ ٱللَّهُ ۚ فَلْيَكْتُبْ وَلْيُمْلِلِ ٱلَّذِى عَلَيْهِ ٱلْحَقُّ وَلْيَتَّقِ ٱللَّهَ رَبَّهُۥ وَلَا يَبْخَسْ مِنْهُ شَيْـًۭٔا ۚ فَإِن كَانَ ٱلَّذِى عَلَيْهِ ٱلْحَقُّ سَفِيهًا أَوْ ضَعِيفًا أَوْ لَا يَسْتَطِيعُ أَن يُمِلَّ هُوَ فَلْيُمْلِلْ وَلِيُّهُۥ بِٱلْعَدْلِ ۚ وَٱسْتَشْهِدُوا۟ شَهِيدَيْنِ مِن رِّجَالِكُمْ ۖ فَإِن لَّمْ يَكُونَا رَجُلَيْنِ فَرَجُلٌۭ وَٱمْرَأَتَانِ مِمَّن تَرْضَوْنَ مِنَ ٱلشُّهَدَآءِ أَن تَضِلَّ إِحْدَىٰهُمَا فَتُذَكِّرَ إِحْدَىٰهُمَا ٱلْأُخْرَىٰ ۚ وَلَا يَأْبَ ٱلشُّهَدَآءُ إِذَا مَا دُعُوا۟ ۚ وَلَا تَسْـَٔمُوٓا۟ أَن تَكْتُبُوهُ صَغِيرًا أَوْ كَبِيرًا إِلَىٰٓ أَجَلِهِۦ ۚ ذَٰلِكُمْ أَقْسَطُ عِندَ ٱللَّهِ وَأَقْوَمُ لِلشَّهَـٰدَةِ وَأَدْنَىٰٓ أَلَّا تَرْتَابُوٓا۟ ۖ إِلَّآ أَن تَكُونَ تِجَـٰرَةً حَاضِرَةًۭ تُدِيرُونَهَا بَيْنَكُمْ فَلَيْسَ عَلَيْكُمْ جُنَاحٌ أَلَّا تَكْتُبُوهَا ۗ وَأَشْهِدُوٓا۟ إِذَا تَبَايَعْتُمْ ۚ وَلَا يُضَآرَّ كَاتِبٌۭ وَلَا شَهِيدٌۭ ۚ وَإِن تَفْعَلُوا۟ فَإِنَّهُۥ فُسُوقٌۢ بِكُمْ ۗ وَٱتَّقُوا۟ ٱللَّهَ ۖ وَيُعَلِّمُكُمُ ٱللَّهُ ۗ وَٱللَّهُ بِكُلِّ شَىْءٍ عَلِيمٌۭ";
+
+const baqara282roman =
+  "yaā'aayuhā al-aḏyna āmanwā 'iiḏā tadāyantum bidaynin 'iilaỳā 'aaǧalin musamanỳ fāktubwhu  walyaktub baynakum kātibun bilʿadli  walā ya'aba kātibun 'aan yaktuba kamā ʿalamahu al-lahu  falyaktub walyumlili al-aḏiỳ ʿalayhi al-ḥaqu walyataqi al-laha rabahu walā yabẖas minhu šayanā  fa'iin kāna al-aḏiỳ ʿalayhi al-ḥaqu safyhanā 'aaw ḍaʿyfanā 'aaw lā yastaṭyʿu 'aan yumila hwa falyumlil walyuhu bilʿadli  wāstašhidwā šahydayni min riǧālikum  fa'iin lam yakwnā raǧulayni faraǧulun wāmra'aatāni miman tarḍawna mina aš-šuhadā'i 'aan taḍila 'iiḥdaỳāhumā fatuḏakira 'iiḥdaỳāhumā al-'auẖraỳā  walā ya'aba aš-šuhadā'u 'iiḏā mā duʿwā  walā tasamwā 'aan taktubwhu ṣaġyranā 'aaw kabyranā 'iilaỳā 'aaǧalihi  ḏālikum 'aaqsaṭu ʿinda al-lahi wa'aaqwamu lilšahaādati wa'aadnaỳā 'aalā tartābwā  'iilā 'aan takwna tiǧaāratan ḥāḍiratan tudyrwnahā baynakum falaysa ʿalaykum ǧunāḥun 'aalā taktubwhā  wa'aašhidwā 'iiḏā tabāyaʿtum  walā yuḍāra kātibun walā šahydun  wa'iin tafʿalwā fa'iinahu fuswqun bikum  wātaqwā al-laha  wayuʿalimukumu al-lahu  wāllahu bikuli šaỳ'in ʿalymun";
+
+const alImran154arab =
+  "ثُمَّ أَنزَلَ عَلَيۡكُم مِّنۢ بَعۡدِ ٱلۡغَمِّ أَمَنَةٗ نُّعَاسٗا يَغۡشَىٰ طَآئِفَةٗ مِّنكُمۡۖ وَطَآئِفَةٞ قَدۡ أَهَمَّتۡهُمۡ أَنفُسُهُمۡ يَظُنُّونَ بِٱللَّهِ غَيۡرَ ٱلۡحَقِّ ظَنَّ ٱلۡجَٰهِلِيَّةِۖ يَقُولُونَ هَل لَّنَا مِنَ ٱلۡأَمۡرِ مِن شَيۡءٖۗ قُلۡ إِنَّ ٱلۡأَمۡرَ كُلَّهُۥ لِلَّهِۗ يُخۡفُونَ فِيٓ أَنفُسِهِم مَّا لَا يُبۡدُونَ لَكَۖ يَقُولُونَ لَوۡ كَانَ لَنَا مِنَ ٱلۡأَمۡرِ شَيۡءٞ مَّا قُتِلۡنَا هَٰهُنَاۗ قُل لَّوۡ كُنتُمۡ فِي بُيُوتِكُمۡ لَبَرَزَ ٱلَّذِينَ كُتِبَ عَلَيۡهِمُ ٱلۡقَتۡلُ إِلَىٰ مَضَاجِعِهِمۡۖ وَلِيَبۡتَلِيَ ٱللَّهُ مَا فِي صُدُورِكُمۡ وَلِيُمَحِّصَ مَا فِي قُلُوبِكُمۡۚ وَٱللَّهُ عَلِيمُۢ بِذَاتِ ٱلصُّدُورِ";
+
+const alImran154roman =
+  "thumma anzala ʿalaykum min baʿdi l-ghami amanatan nuʿāsan yaghshā ṭāifatan minkum waṭāifatun qad ahammathum anfusuhum yaẓunnūna bil-lahi ghayra l-ḥaqi ẓanna l-jāhiliyati yaqūlūna hal lanā mina l-amri min shayin qul inna l-amra kullahu lillahi yukh'fūna fī anfusihim mā lā yub'dūna laka yaqūlūna law kāna lanā mina l-amri shayon mā qutil'nā hāhunā qul law kuntum fī buyūtikum labaraza alladhīna kutiba ʿalayhimu l-qatlu ilā maḍājiʿihim waliyabtaliya l-lahu mā fī ṣudūrikum waliyumaḥḥiṣa mā fī qulūbikum wal-lahu ʿalīmun bidhāti l-ṣudūri";
+
+const arabLetters = {
+  ء: "'", // hamza,
+  ٱ: "a", //aa
+  آ: "'a", // madda
+  أ: "a", // hamza on alif
+  ؤ: "v", // hamza on waw
+  إ: "i", // hamza on alif
+  ئ: "y", // hamza on ya
+  ا: "a", // alif
+  ب: "b", // ba
+  ت: "t", // ta
+  ة: "t", //t
+  ث: "th", // tha
+  ج: "j", // jim
+  ح: "h", // ha
+  خ: "kh", // kha
+  د: "d", // dal
+  ذ: "th", // thal
+  ر: "r", // ra
+  ز: "z", // zayn
+  س: "s", // sin
+  ش: "sh", // shin
+  ص: "s", // sad
+  ض: "d", // dad
+  ط: "t", // ta
+  ظ: "z", // za
+  ع: "a", // ain
+  غ: "gh", // ghain
+  ف: "f", // fa
+  ق: "q", // qaf
+  ك: "k", // kaf
+  ل: "l", // lam
+  م: "m", // mim
+  ن: "n", // nun
+  ه: "h", // ha
+  و: "w", // waw
+  ي: "y", // ya
+  ى: "ỳ", //ya
+  "َ": "a", // fatha
+  "ُ": "u", // damma
+  "ِ": "i", // kasra
+  "ً": "an", // fathatan
+  "ٌ": "un", // dammatan
+  "ُُ": "un", // dammatan
+  "ٍ": "in", // kasratan
+  "ْ": "", // sukun
+  "ّ": "²", // double stop
+};
+
+const lettersColors = {
+  ء: "PeachPuff", // hamza
+  ٱ: "MintCream", //aa
+  آ: "BlanchedAlmond", // madda
+  أ: "Aqua", // hamza on alif
+  ؤ: "LightCoral", // hamza on waw
+  إ: "DarkTurquoise", // hamza on alif
+  ئ: "DarkSalmon", // hamza on ya
+  ا: "Crimson",
+  ب: "Navy",
+  ت: "Coral",
+  ث: "Olive",
+  ج: "Teal",
+  ح: "Maroon",
+  خ: "SkyBlue",
+  د: "Sienna",
+  ذ: "Turquoise",
+  ر: "Indigo",
+  ز: "Salmon",
+  س: "ForestGreen",
+  ش: "Blue", //shin
+  ص: "SlateGray",
+  ض: "Gold",
+  ط: "DarkOrange",
+  ظ: "SteelBlue",
+  ع: "DarkKhaki",
+  غ: "Orchid",
+  ف: "MediumPurple",
+  ق: "DarkSlateBlue",
+  ك: "Tomato",
+  ل: "MediumSeaGreen",
+  م: "DarkTurquoise",
+  ن: "MediumVioletRed",
+  ه: "DodgerBlue",
+  و: "DarkOliveGreen",
+  ي: "MediumAquamarine",
+  ى: "Lavender", //ya
+};
+
+const aralphabet = {
+  ء: "(h)amza", // (h)amza,
+  ٱ: "(w)alif", // wasla on alif
+  آ: "(m)alif", // madda on alif
+  أ: "(h)alif", // hamza on alif
+  ؤ: "(h)waw", // hamza on waw
+  إ: "ee", // hamza on alif
+  ئ: "(h)ya", // hamza on ya
+  ا: "alif",
+  ب: "ba",
+  ت: "ta",
+  ث: "tha",
+  ج: "jim",
+  ح: "ha",
+  خ: "kha",
+  د: "dal",
+  ذ: "thal",
+  ر: "ra",
+  ز: "zayn",
+  س: "sin",
+  ش: "shin",
+  ص: "sad",
+  ض: "dad",
+  ط: "ta",
+  ظ: "za",
+  ع: "ain",
+  غ: "ghain",
+  ف: "fa",
+  ق: "qaf",
+  ك: "kaf",
+  ل: "lam",
+  م: "meem",
+  ن: "noon",
+  ه: "ha",
+  و: "waw",
+  ي: "ya",
+  ى: "ỳa",
+};
+
+const romanColors = {
+  "'": "PeachPuff", // hamza,
+  A: "MintCream", //aa
+  A: "BlanchedAlmond'", // madda
+  O: "Aqua", // hamza on alif
+  V: "LightCoral", // hamza on waw
+  I: "DarkTurquoise", // hamza on alif
+  Y: "DarkSalmon", // hamza on ya
+  a: "Crimson",
+  b: "Navy",
+  t: "Coral",
+  ð: "Olive", //th
+  j: "Teal",
+  h: "Maroon",
+  x: "SkyBlue",
+  d: "Sienna",
+  ž: "Turquoise", //z'
+  r: "Indigo",
+  z: "Salmon",
+  s: "ForestGreen",
+  š: "Blue", //shin
+  c: "SlateGray",
+  Ð: "Gold", //dod
+  t: "DarkOrange", //t^
+  Z: "SteelBlue", //z^
+  æ: "DarkKhaki", //a'
+  g: "Orchid",
+  f: "MediumPurple",
+  q: "DarkSlateBlue",
+  k: "Tomato",
+  l: "MediumSeaGreen",
+  m: "DarkTurquoise",
+  n: "MediumVioletRed",
+  H: "DodgerBlue", //ha
+  w: "DarkOliveGreen",
+  y: "MediumAquamarine",
+};
+
+function colorizeText(text, lettersColors) {
+  let html = "";
+  let colorIndex = 0;
+
+  text.split(" ").forEach((word) => {
+    let wordHtml = "";
+
+    for (let i = 0; i < word.length; i++) {
+      let letter = word[i];
+      let color = lettersColors[letter];
+
+      if (!color) {
+        colorIndex = (colorIndex + 1) % Object.values(lettersColors).length;
+        color = Object.values(lettersColors)[colorIndex];
+      }
+
+      wordHtml += `<span style="color:${color}">${letter}</span>`;
+    }
+
+    html += `${wordHtml} `;
+  });
+  return html;
+}
+
+function generateTable(lettersColors, alphabet) {
+  let table = "<div class='grid-container'>";
+
+  for (const [letter, color] of Object.entries(lettersColors)) {
+    let transliteration = "";
+
+    transliteration = alphabet[letter];
+
+    table += `<div class="grid-item">
+          <div class="letter" style="color:${color}" onclick="toggleFade('${letter}')">${letter}</div>
+          <div class="translit">${transliteration}</div>
+        </div>`;
+  }
+
+  table += "</div>";
+
+  return table;
+}
+
+function toggleFade(translit) {
+  const spans = document.querySelectorAll(".output span");
+  const filteredSpans = Array.from(spans).filter((span) =>
+    span.textContent.includes(translit)
+  );
+
+  filteredSpans.forEach((span) => {
+    if (span.style.animation) {
+      span.style.animation = "";
+    } else {
+      span.style.animation = "fade 1s infinite";
+    }
+  });
+}
+
+function romanizeArabic(text, arabLetters) {
+  const chars = text.toLowerCase().split("");
+  let color = "";
+  const transliterated = chars
+    .filter((char) => arabLetters.hasOwnProperty(char) || char === " ")
+    .map((char, index, array) => {
+      const prevChar = array[index - 1];
+      const nextChar = array[index + 1];
+      const letter = arabLetters[char] || char;
+      if (lettersColors[char]) {
+        color = lettersColors[char];
+      } else if (lettersColors[prevChar]) {
+        color = lettersColors[prevChar];
+      } else if (lettersColors[nextChar]) {
+        color = lettersColors[nextChar];
+      }
+      return `<span style="color: ${color}">${letter}</span>`;
+    })
+    .join("");
+  return transliterated;
+}
+
+function romanizeArabic2(text, arabLetters) {
+  let chars = text.split("");
+  let color = "";
+  let result = "";
+  let prevChar = "";
+  let prevPrevChar = "";
+  for (let i = 0; i < chars.length; i++) {
+    let char = chars[i];
+    let nextChar = chars[i + 4];
+    let letter = arabLetters[char] || char;
+    if (char === "ّ" && prevPrevChar !== "") {
+      let doubledLetter = prevPrevChar.repeat(1);
+      result += `<span style="color: ${color}">${doubledLetter}</span>`;
+    } else if (arabLetters.hasOwnProperty(char) || char === " ") {
+      if (lettersColors[char]) {
+        color = lettersColors[char];
+      } else if (lettersColors[prevChar]) {
+        color = lettersColors[prevChar];
+      } else if (lettersColors[nextChar]) {
+        color = lettersColors[nextChar];
+      }
+      result += `<span style="color: ${color}">${letter}</span>`;
+    }
+    prevPrevChar = prevChar;
+    prevChar = letter;
+  }
+  return result;
+}
+
+document.querySelector(".table").innerHTML = generateTable(
+  lettersColors,
+  aralphabet
+);
+
+document.querySelector(".output").innerHTML = colorizeText(
+  alImran154arab,
+  lettersColors
+);
+
+document.querySelector(".two").innerHTML = romanizeArabic(
+  alImran154arab,
+  arabLetters
+);
