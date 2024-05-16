22be864e06dd91d98643f34b2650f42b7858af4e
Muhiddin <muhiddinjv@gmail.com>
2024-05-16 19:08:04 +0500

    quick  commit

--- components/Surahs.js
+++ components/Surahs.js
@@ -14,7 +14,7 @@ function Surahs() {
   return (
     <div className={`${ theme === "dark" ? "bg-gray-800 text-slate-300" : "bg-gray-100 text-slate-800"} grid grid-cols-3 gap-3 p-4 min-h-screen w-screen items-center justify-center`}>
         {chapters?.map(chapter => {
-          return (<span onClick={()=>setSurahNum(chapter.id)} className="px-4 py-2 border rounded">
+          return (<span onClick={()=>setSurahNum(chapter.id)} className="px-4 py-2 text-center border rounded">
             <Link href={`/${chapter.id}`}>Start {chapter.id}</Link>
           </span>)
         })}
