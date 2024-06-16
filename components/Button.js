0455cae82ae54adb97ed2edb4ce7440ae85df79f
muhiddin <muhiddinjv@gmail.com>
2024-06-16 19:51:52 +0500

    remove old files

--- components/Button.js
+++
@@ -1,6 +0,0 @@
-function Button({theme, onClick, text}){
-  return (
-    <button onClick={onClick} className={`${theme === 'dark' ? 'bg-gray-600' : 'bg-gray-300'} hover:bg-gray-400 size-8 rounded`}>{text}</button>
-  );
-}
-
