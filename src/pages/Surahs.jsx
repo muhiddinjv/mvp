e87c5e5dda277dfa0dfec30a6ed51122c36c690c
muhiddin <muhiddinjv@gmail.com>
2024-06-18 17:33:32 +0500

    update sajda sort icon

--- src/pages/Surahs.jsx
+++ src/pages/Surahs.jsx
@@ -1,5 +1,5 @@
 import React from'react';
-import { sajdaSurahs, useTheme } from '../hooks';
+import { useTheme } from '../hooks';
 import { GlobalContext } from '../main';
 import { Link } from 'react-router-dom';
 
@@ -31,7 +31,7 @@ function Surahs() {
     <div className={`${ theme === "dark" ? "bg-gray-800 text-slate-300" : "bg-gray-100 text-slate-800"} grid grid-cols-1 gap-3 p-4 min-h-screen w-screen items-center justify-center`}>
         <div className='flex text-center cursor-pointer'>
           <span onClick={()=>handleSortBy('id')} className='border rounded-l w-full py-2'>Surah {sortType === 'id' && sortOrder ? '⇩' : '⇧'}</span>
-          <span onClick={()=>handleSortBy('sajda')} className='border rounded-r w-full py-2'>Sajda {sortType === 'sajda' && sortOrder ? '⇩' : '۩'}</span>
+          <span onClick={()=>handleSortBy('sajda')} className='border rounded-r w-full py-2'>Sajda {sortType === 'sajda' && sortOrder ? '۩' : '⇧'}</span>
           <span onClick={()=>handleSortBy('words')} className='border rounded-r w-full py-2'>Word {sortType === 'words' && sortOrder ? '⇩' : '⇧'}</span>
         </div>
         {chapters?.map(chapter => {
