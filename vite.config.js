df8f84f800ffa807afd0daa733f2531d4fe1baa3
Muhiddin <muhiddinjv@gmail.com>
2024-07-18 12:07:13 +0500

    make the app PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,52 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+let faviconURL = "/img/android-chrome-512x512.svg";
+
 export default defineConfig({
-  plugins: [react(),VitePWA({ registerType: 'autoUpdate' })],
-  // build: { outDir: 'build' },
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["/public/*"],
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest,json}"],
+      },
+      manifest: {
+        name: "Book Word By Word",
+        short_name: "bwbw",
+        description: "I am a simple pwa vite app",
+        scope: "/",
+        start_url: "index.html",
+        background_color: "#f0e7db",
+        theme_color: "#171717",
+        display: "standalone",
+        orientation: "any",
+        prefer_related_applications: false,
+        // install your native app on users device directly from the app store
+        // related_applications: [{
+        //   platform: "play",
+        //   id: "com.google.samples.apps.iosched"
+        // }],
+        icons: [
+          {
+            src: faviconURL,
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+          {
+            src: faviconURL,
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
